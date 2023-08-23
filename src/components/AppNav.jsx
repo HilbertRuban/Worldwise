75146ff6019f1d6adf3cbcfa247a83f83a25a56f
Hilbert Ruban <hilbert.r@targetbay.com>
2023-08-24 02:02:16 +0530

    added static ui of cities and countries in sidebar

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styles from './AppNav.module.css'
 
 const AppNav = () => {
   return (
-    <nav>Worldwise</nav>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to='cities'>Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to='countries'>Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
--- src/components/Mao.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Mao = () => {
-  return (
-    <div>Mao</div>
-  )
-}
-
-export default Mao
